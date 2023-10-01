51876e62688357e6b73649772683cbdef3573f5e
Alejandro Ramos <ramosa@berea.edu>
2023-09-30 21:14:49 -0400

    Made Colleges.jsx

--- Reserve_App_Frontend/src/App.jsx
+++ Reserve_App_Frontend/src/App.jsx
@@ -1,16 +1,33 @@
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Pages/home';
-import BereaCollege from './components/Pages/berea-college';
+import {
+  BereaCollege,
+  YaleCollege,
+  HarvardCollege,
+  DukeCollege,
+  KentuckyCollege,
+  AccraCollege,
+  SanDiegoCollege,
+  DallasCollege,
+} from './components/Pages/Colleges'; // Import the college components
 
 const App = () => {
- return (
+  return (
     <>
-       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/berea-college" element={<BereaCollege />} />
-       </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/berea-college" element={<BereaCollege />} />
+        <Route path="/yale" element={<YaleCollege />} />
+        <Route path="/harvard" element={<HarvardCollege />} />
+        <Route path="/duke" element={<DukeCollege />} />
+        <Route path="/kentucky" element={<KentuckyCollege />} />
+        <Route path="/accra" element={<AccraCollege />} />
+        <Route path="/san-diego" element={<SanDiegoCollege />} />
+        <Route path="/dallas" element={<DallasCollege />} />
+      </Routes>
     </>
- );
+  );
 };
 
 export default App;
